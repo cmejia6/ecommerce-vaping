aec294f5bf8448b6c2c31b04bd47bea39135df4d
Alex <carlos.mejia1342@gmail.com>
2019-02-10 17:45:53 -0800

    Slider

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    items: 1,
+    autoplay: 1000
+})
+/*var siteCarousel = function () {
+    if ( $('.nonloop-block-13').length > 0 ) {
+        $('.nonloop-block-13').owlCarousel({
+        center: false,
+        items: 1,
+        loop: true,
+            stagePadding: 0,
+        margin: 0,
+        autoplay: true,
+        nav: true,
+            navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+        responsive:{
+        600:{
+            margin: 0,
+          items: 1
+        },
+        1000:{
+            margin: 0,
+            stagePadding: 0,
+          items: 1
+        },
+        1200:{
+            margin: 0,
+            stagePadding: 0,
+          items: 1
+        }
+        }
+        });
+    }
+
+    $('.slide-one-item').owlCarousel({
+    center: false,
+    items: 1,
+    loop: true,
+    stagePadding: 0,
+    margin: 0,
+    autoplay: true,
+    pauseOnHover: false,
+    nav: true,
+    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  });
+};
+siteCarousel();*/
